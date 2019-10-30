26d9dc4bdf59aafcc07f701e87e616cb5d87f882
shj <gurwls1240@naver.com>
2019-10-30 17:58:16 +0900

    1030f

--- 05_Django/03_django_crud/articles/urls.py
+++ 05_Django/03_django_crud/articles/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('<int:article_pk>/', views.detail),
     path('create/', views.create),
     path('new/', views.new),
     path('index/', views.index)
